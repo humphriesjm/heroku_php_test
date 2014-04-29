d71e904103aae273f8a9883f9d0431682176718c
Jason Humphries <humphriesj@gmail.com>
2014-04-29 14:07:13 -0400

    adds Monolog logging to index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
-echo "Hello World";
+require('vendor/autoload.php');
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel to STDERR
+$log = new Logger('name');
+$log->addWarning("Running a beta version of Heroku's PHP support.");
+
+echo "Jason can do php too.";
 
 ?>
